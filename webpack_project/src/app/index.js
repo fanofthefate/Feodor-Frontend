08259c2b9d015e02845d239540939e4d054ba233
Dmitry Blinov <dedtorpedodet@gmail.com>
2019-12-27 23:45:48 +0400

    LK

---
+++ webpack_project/src/app/index.js
@@ -0,0 +1,50 @@
+import jQuery from 'jquery';
+import popper from 'popper.js';
+import bootstrap from 'bootstrap';
+import Swiper from 'swiper';
+
+//Font Avesome
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+import '../style/app.scss';
+
+
+(function ($) {
+
+  $('.card__exam').on('click', function(){
+   $('.personal-cabinet__home-page').hide();
+   $('.personal-cabinet__interface').show();
+ });
+
+  let  schoolItemsSlider = new Swiper('.school-items-slider', {
+    slidesPerView: 5,
+    spaceBetween: 20,
+    mousewheel: {
+      sensitivity: 1
+    },
+    on:{
+      observerUpdate: function(){
+        this.updateSize();
+        console.log('update')
+      }
+    }
+  });
+
+  //Открывает левое меню https://prnt.sc/q9zp9n
+
+  $('[data-widget="pushmenu"]').on('click', function(){
+    function toggleBurger(){
+      var bmenu = document.querySelector('.bmenu');
+      if(bmenu.className.match('active'))
+        bmenu.className = 'bmenu';
+      else
+        bmenu.className= 'bmenu active';
+    }
+    toggleBurger();
+    $('body').toggleClass('main-sidebar-collapse');
+  });
+
+})(jQuery);
---
+++ webpack_project/webpack.config.js
@@ -0,0 +1,79 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
+const CopyPlugin = require('copy-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+  entry: __dirname + "/src/app/index.js", // webpack entry point. Module to start building dependency graph
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Folder to store generated bundle
+    filename: 'bundle.js'  // Name of generated bundle after build
+  },
+  module: { 
+   rules: [ 
+   {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+     loader: 'babel-loader',
+     options: {
+      presets: ['@babel/preset-env']
+    }
+  }
+},
+{
+  test: /\.(png|jpg|gif)$/,
+  use: [
+  {
+   loader: 'file-loader',
+   options: {name: 'img/[name].[ext]'}  
+ }
+ ]
+},
+{
+  test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/, 
+  use: [
+  'file-loader',
+  ],
+},
+{
+  test: /\.(sass|scss)$/,
+  use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+          }, {
+            loader: "css-loader" // translates CSS into CommonJS
+          }, 
+
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+            	plugins: function () {
+            		return [
+            		require('precss'),
+            		require('autoprefixer')
+            		];
+            	}
+            }
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          },
+          ]
+        }
+        ]
+      },
+  plugins: [  // Array of plugins to apply to build chunk
+  new HtmlWebpackPlugin({
+  	template: __dirname + "/src/public/index.html",
+  	inject: 'body'
+  }),
+  new CopyPlugin([
+        { from: './src/public/img', to: './img'}
+    ])
+  ],
+  devServer: {  // configuration for webpack-dev-server
+      contentBase: './src/public',
+      port: 7700 // port to run dev-server
+    } 
+  };
