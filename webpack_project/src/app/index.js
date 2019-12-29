eaece3ec1ed1bfc6f0cc911bbf9b8acfe2da2f40
Dmitry Blinov <dedtorpedodet@gmail.com>
2019-12-30 01:11:58 +0400

    Toggle Messanger Wrapper

--- webpack_project/src/app/index.js
+++ webpack_project/src/app/index.js
@@ -44,27 +44,28 @@ import '../style/app.scss';
 
   function _autoHeightDialogHistory(){
     let msgDlg__header = document.querySelector('.messanger__dialog__header'),
-        msgDlg__footer = document.querySelector('.messanger__dialog__footer'),
-        msgDlg__history = document.querySelector('.messanger__dialog__history'),
-        msgDlg__wrapper = document.querySelector('.messanger-wrapper');
+    msgDlg__footer = document.querySelector('.messanger__dialog__footer'),
+    msgDlg__history = document.querySelector('.messanger__dialog__history'),
+    msgDlg__wrapper = document.querySelector('.messanger-wrapper');
 
     let msgDlg__wrapper_h = msgDlg__wrapper.offsetHeight,
-        msgDlg__header_h = msgDlg__header.offsetHeight,
-        msgDlg__footer_h = msgDlg__footer.offsetHeight,
-        msgDlg__history_h = msgDlg__wrapper_h - msgDlg__footer_h - msgDlg__header_h;
-        console.log(msgDlg__wrapper_h);
-        console.log(msgDlg__header_h);
-        console.log(msgDlg__footer_h);
-        console.log(msgDlg__history_h);
-        msgDlg__history.style.height = msgDlg__wrapper_h - msgDlg__footer_h - msgDlg__header_h+'px';
+    msgDlg__header_h = msgDlg__header.offsetHeight,
+    msgDlg__footer_h = msgDlg__footer.offsetHeight,
+    msgDlg__history_h = msgDlg__wrapper_h - msgDlg__footer_h - msgDlg__header_h;
+    msgDlg__history.style.height = msgDlg__wrapper_h - msgDlg__footer_h - msgDlg__header_h+'px';
   }
 
-_autoHeightDialogHistory();
-window.onresize = function() {
+  _autoHeightDialogHistory();
+  window.onresize = function() {
     _autoHeightDialogHistory();
-};
+  };
 
-
+$('[data-target="messanger"]').on('click' , function(){
+  let target = $(this).attr('href');
+  $('.messanger-wrapper').toggleClass('open');
+  $(target).toggleClass('active');
+  $(this).toggleClass('active');
+});
 })(jQuery);
 
 
