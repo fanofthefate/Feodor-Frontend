22f1165210eb3044768f42f4b459db4e767bd4a4
Dmitry Blinov <dedtorpedodet@gmail.com>
2020-01-24 01:29:33 +0400

    Sign

--- webpack_project/src/app/index.js
+++ webpack_project/src/app/index.js
@@ -1,58 +1,29 @@
-import jQuery from 'jquery';
+import $ from 'jquery';
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
 import Swiper from 'swiper';
 
 //Font Avesome
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+import '@fortawesome/fontawesome-free/js/all'
 
+import 'bootstrap-select';
 
 import '../style/app.scss';
 
-
-function _autoHeightDialogHistory(){
-  let msgDlg__header = document.querySelector('.messanger__dialog__header'),
-  msgDlg__footer = document.querySelector('.messanger__dialog__footer'),
-  msgDlg__history = document.querySelector('.messanger__dialog__history'),
-  msgDlg__wrapper = document.querySelector('.messanger-wrapper');
-
-  let msgDlg__wrapper_h = msgDlg__wrapper.offsetHeight,
-  msgDlg__header_h = msgDlg__header.offsetHeight,
-  msgDlg__footer_h = msgDlg__footer.offsetHeight,
-  msgDlg__history_h = msgDlg__wrapper_h - msgDlg__footer_h - msgDlg__header_h;
-  msgDlg__history.style.height = msgDlg__wrapper_h - msgDlg__footer_h - msgDlg__header_h+'px';
-}
-
 (function ($) {
-  $('.card__exam').on('click', function(){
-   $('.personal-cabinet__home-page').hide();
-   $('.personal-cabinet__interface').show();
- });
 
-$('[data-toggle="popover"]').popover()
+  let sidebarRight = $('.sidebar-right');
 
-$('[data-action="action-menu-more"]').on('mouseover', function(){
-  if ($(this).hasClass('show') == false){
-     $(this).addClass('show');
-  }
-});
-
-$('[data-action="action-menu-more"]').on('mouseout', function(){
-  if ($(this).hasClass('show')){
-     $(this).removeClass('show');
-  }
-});
-
-
-  let  schoolItemsSlider = new Swiper('.school-items-slider', {
-    slidesPerView: 5,
+  $('[data-toggle="popover"]').popover();
+  $('select').selectpicker();
+  $('[data-toggle="tooltip"]').tooltip();
+  $('#selectPartner').selectpicker('hide'); //Скрываем по умолчанию выбор типа регестрации
+  let  schoolItemsSlider = new Swiper('.courses-level-slider', {
+    slidesPerView: 3,
     spaceBetween: 20,
     navigation: {
-      nextEl: '.school-items-slider__navigation__next',
-    prevEl: '.school-items-slider__navigation__prev',
+      nextEl: '.courses-level__navigation__next',
+      prevEl: '.courses-level__navigation__prev',
     },
     mousewheel: {
       sensitivity: 1
@@ -64,6 +35,9 @@ $('[data-action="action-menu-more"]').on('mouseout', function(){
     }
   });
 
+  $('.messanger-control__close').on('click', function(){
+    $('.messanger-wrapper').toggleClass('open');
+  });
 
   $('[data-sidebar]').on('click', function(){
     let sidebar_push = $(this).data('sidebar');
@@ -73,44 +47,58 @@ $('[data-action="action-menu-more"]').on('mouseout', function(){
     }
   });
 
-  $('[data-action]').on('click' , function(){
+  $('[data-toggle="pill"]').on('click', function(e){
+    let navLink = $('[data-toggle="nav-link"]');
+    let navLinkTarget = $('[data-toggle="nav-link"]').attr('href');
+    let wrapper = $('.messanger-wrapper');
+
+    if (!wrapper.hasClass('open')) {
+      wrapper.addClass('open');
+    }   
+
+    if (!sidebarRight.hasClass('sidebar-mini')) {
+      sidebarRight.addClass('sidebar-mini');
+    }
+
+  });
+  $('[data-type-sign]').on('click', function(){
+    let activeTypeSign = $(this).data('type-sign');
+
+  });
+
+  $('[data-action]').on('click' , function(e){
     let target_type = $(this).data('action'),
     target = $(this).data('target');
-
     switch(target_type){
-      case 'messanger':
-      $('.messanger-wrapper').toggleClass('open');
-      $(target).toggleClass('active');
-      $(this).toggleClass('active');
-      break;
       case 'dialog':
       $(target).show();
-      _autoHeightDialogHistory();
-      break;
-      case 'back-link':
-      $(target).hide();
       break;
       case 'switch-logo':
       $('.sign-logo').hide();
       $(target).show();
       break;
-      case 'sign-submit':
-        $('.sign__form__phone').hide(); 
-        $('.sign__form__verifiled').show(); 
-        $('#btnSignSubmit').html('Отправить <i class="fas fa-arrow-right"></i>'); 
-        $('[name="typeSignForm"]').attr('disabled', 'disabled'); 
-
-
+      case 'formConfirm':
+      $('#formField').hide(); //Скрыли поля для заполнения 
+      $('#formConfirm').show();  //Показали подтверждение по СМС
+      break;
+      case 'chose-type-sign': // Выбрать тип регистрации
+      if ($('.sign-teacher').hasClass('active')) {
+        $('#selectPartner').selectpicker('show');
+        $('#SignLogoStudent').hide();
+        $('#SignLogoTeacher').show();
+      } else{
+        $('#selectPartner').selectpicker('hide');
+        $('#selectPartner').val('noselect'); // Сброс селекта
+        $('#selectPartner').selectpicker('refresh'); // Сброс селекта
+        $('#SignLogoTeacher').hide();
+        $('#SignLogoStudent').show();
+        
+      }
+      break;
     }
   });
 
-  _autoHeightDialogHistory();
-
 })(jQuery);
-
-window.onresize = function() {
-  _autoHeightDialogHistory();
-};
 
 
 
